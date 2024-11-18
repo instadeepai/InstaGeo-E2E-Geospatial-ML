ba186d3635f0d658ec48d4ed3a588b39a90f1299
rym-oualha <r.oualha@instadeep.com>
2024-11-18 22:19:24 +0100

    feat: add some functions to handle s2 data

--- instageo/data/s2_utils.py
+++ instageo/data/s2_utils.py
@@ -19,19 +19,21 @@
 
 """Utility Functions for Reading and Processing Sentinel-2 Dataset."""
 
-# import zipfile
 import multiprocessing
 import os
+import shutil
 import subprocess
 import time
+import zipfile
 from datetime import datetime
-
-# import shutil
-# import numpy as np
-# import rasterio
 from typing import Dict, List, Optional, Tuple
 
+import numpy as np
+import rasterio
 import requests  # type: ignore
+import rioxarray
+import xarray as xr
+from rasterio.crs import CRS
 
 
 def get_access_and_refresh_token(
@@ -254,7 +256,7 @@ def parallel_downloads_s2(
     download_info_list: List[Tuple[str, str, str]],
     output_directory: str,
 ) -> None:
-    """Parallel download for Sentinel-2 Tiles .
+    """Parallel download for Sentinel-2 Tiles.
 
     Manages parallel downloads of products using multiprocessing, handling token generation
     and refresh.
@@ -293,3 +295,215 @@ def parallel_downloads_s2(
                 for download_info in download_info_list
             ],
         )
+
+
+def unzip_file(zip_file: str, output_dir: str) -> None:
+    """Unzip S2 products.
+
+    Extracts the contents of a zip file to the specified output directory.
+
+    Args:
+        zip_file (str): The path to the zip file to be extracted.
+        output_dir (str): The directory where the contents of the zip file will be extracted.
+
+    Returns:
+        None: This function doesn't return any value. It prints messages indicating the status.
+
+    Raises:
+        zipfile.BadZipFile: If the provided file is not a valid zip file.
+        FileNotFoundError: If the zip file is not found at the given path.
+    """
+    try:
+        with zipfile.ZipFile(zip_file, "r") as zip_ref:
+            zip_ref.extractall(output_dir)
+            print(f"Extracted: {zip_file} to {output_dir}")
+        os.remove(zip_file)
+        print(f"Deleted zip file: {zip_file}")
+    except zipfile.BadZipFile:
+        print(f"Error: {zip_file} is not a valid zip file")
+    except FileNotFoundError:
+        print(f"File not found: {zip_file}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def get_band_files(tile_name: str, full_tile_id: str, output_directory: str) -> None:
+    """Handle band files.
+
+    Navigates through the folders to find the bands in the IMG_DATA/R20m folder,
+    moves them to the tile folder, and deletes unnecessary files.
+
+    Args:
+        tile_name (str): The tile name.
+        full_tile_id (str): The base folder where the unzipped contents are stored.
+        output_directory (str): The root directory where the tile folders are located.
+
+    Returns:
+    None: This function doesn't return any value. It performs file operations and prints messages.
+    """
+    bands_needed = ["B02", "B03", "B04", "B8A", "B11", "B12", "SCL"]
+    tile_folder = os.path.join(output_directory, tile_name)
+    print(f"Main directory: {tile_folder}")
+
+    base_dir = os.path.join(tile_folder, full_tile_id, "GRANULE")
+    print(f"Base directory: {base_dir}")
+
+    if os.path.isdir(base_dir):
+        granule_folders = os.listdir(base_dir)
+        if granule_folders:
+            granule_folder = granule_folders[0]
+            img_data_dir = os.path.join(base_dir, granule_folder, "IMG_DATA", "R20m")
+
+            if os.path.isdir(img_data_dir):
+                for file_name in os.listdir(img_data_dir):
+                    for band in bands_needed:
+                        if band in file_name:
+                            full_file_path = os.path.join(img_data_dir, file_name)
+                            target_file_path = os.path.join(tile_folder, file_name)
+
+                            shutil.move(full_file_path, target_file_path)
+                            print(f"Moved {file_name} to {tile_folder}")
+
+                shutil.rmtree(os.path.join(tile_folder, full_tile_id))
+                print(f"Deleted unneeded directories for {full_tile_id}")
+            else:
+                print(f"R20m folder not found in {granule_folder}")
+        else:
+            print(f"No subfolder found in GRANULE for {full_tile_id}")
+    else:
+        print(f"GRANULE folder not found in {full_tile_id}")
+
+
+def count_valid_pixels(scl_band_path: str) -> int:
+    """Valid pixels count.
+
+    Count valid pixels in the SCL band, ignoring pixels with class 0 (No Data).
+
+    Args:
+        scl_band_path (str): The file path to the SCL band.
+
+    Returns:
+    int: The number of valid pixels.
+    """
+    with rasterio.open(scl_band_path) as src:
+        scl_data = src.read(1)  # Read the SCL band data as a 2D numpy array
+        valid_pixels = np.count_nonzero(scl_data != 0)
+        print("valid_pixels for ", scl_band_path, valid_pixels)
+    return valid_pixels
+
+
+def find_scl_file(
+    output_path: str, tile_name: str, acquisition_date: datetime
+) -> Optional[str]:
+    """SCL file finder.
+
+    Find the SCL file matching the acquisition date.
+
+    Args:
+        output_path (str): Path to the tile's folder.
+        tile_name (str): Name of the tile.
+        acquisition_date (datetime): Acquisition date for the product.
+
+    Returns:
+    Optional[str]: Path to the matching SCL file, or None if not found.
+    """
+    acquisition_date_str = acquisition_date.strftime("%Y%m%d")
+
+    for file_name in os.listdir(output_path):
+        if file_name.endswith("_SCL_20m.jp2") and tile_name in file_name:
+            if acquisition_date_str in file_name:
+                return os.path.join(output_path, file_name)
+
+    return None
+
+
+def open_mf_jp2_dataset(
+    band_folder: str, num_steps: int, mask_cloud: bool, water_mask: bool
+) -> tuple[xr.Dataset | None, CRS | None]:
+    """Handle JP2 data.
+
+    Open multiple JP2 files as an xarray Dataset and optionally apply filtering for water
+      and clouds.
+
+    Args:
+        band_folder (str): Path to the folder where the bands and SCL band are stored.
+        num_steps (int): Number of timestamps or steps, indicating how many sets of bands
+        are expected.
+        mask_cloud (bool): Whether to apply cloud filtering (classes 8 and 9).
+        water_mask (bool): Whether to apply water filtering (class 6).
+
+    Returns:
+        (xr.Dataset | None, CRS | None): A tuple of xarray Dataset combining data from all the
+            provided JP2 files and its CRS, or (None, None) if the folder is invalid.
+    """
+    if not os.path.exists(band_folder):
+        print(f"Folder '{band_folder}' does not exist. Skipping...")
+        return None, None
+
+    band_crs = None
+
+    band_files = [
+        os.path.join(band_folder, f)
+        for f in os.listdir(band_folder)
+        if f.endswith(".jp2") and "SCL" not in f
+    ]
+    scl_band_files = [
+        os.path.join(band_folder, f)
+        for f in os.listdir(band_folder)
+        if "SCL" in f and f.endswith(".jp2")
+    ]
+
+    if len(band_files) % 6 != 0:
+        print(f"Unexpected number of band files in '{band_folder}': {len(band_files)}")
+        return None, band_crs
+
+    expected_band_count = num_steps * 6  # Assuming 6 bands per timestamp
+    if len(band_files) < expected_band_count:
+        print(
+            f"Not enough band files in '{band_folder}'. Expected at least {expected_band_count}, "
+            f"found {len(band_files)}."
+        )
+        return None, band_crs
+
+    if len(scl_band_files) != num_steps:
+        print(f"Skipping folder '{band_folder}' - missing SCL bands for each timestamp")
+        return None, band_crs
+
+    band_files.sort()
+    bands_list = []
+
+    for band_file in band_files:
+        if os.path.getsize(band_file) > 0:
+            band_data = rioxarray.open_rasterio(band_file)
+            bands_list.append(band_data)
+            if band_crs is None:
+                band_crs = band_data.rio.crs
+        else:
+            print(f"Skipping empty band file: {band_file}")
+
+    if not bands_list:
+        print(f"Skipping folder '{band_folder}' - no valid band files found")
+        return None, band_crs
+
+    bands_dataset = xr.concat(bands_list, dim="band")
+
+    scl_band_files.sort()
+    scl_data = []
+
+    for scl_path in scl_band_files:
+        with rasterio.open(scl_path) as scl_src:
+            scl_data.append(scl_src.read(1))
+
+    scl_data = np.stack(scl_data, axis=0)
+
+    if water_mask:
+        water_mask_array = np.isin(scl_data, [6]).astype(np.uint8)  # Class 6 for water
+        bands_dataset = bands_dataset.where(water_mask_array == 0)
+
+    if mask_cloud:
+        cloud_mask_array = np.isin(scl_data, [8, 9]).astype(
+            np.uint8
+        )  # Classes 8 and 9 for clouds
+        bands_dataset = bands_dataset.where(cloud_mask_array == 0)
+
+    return bands_dataset, band_crs
