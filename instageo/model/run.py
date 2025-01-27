37e30850464adc246d637574162446d8aebe5b7d
Ibrahim Salihu Yusuf <sibrahim1396@gmail.com>
2025-01-27 09:01:35 +0200

    fix: add output_dir for saving predictions

--- instageo/model/run.py
+++ instageo/model/run.py
@@ -708,7 +708,7 @@ def main(cfg: DictConfig) -> None:
     # TODO: Add support for chips that are greater than image size used for training
     elif cfg.mode == "chip_inference":
         check_required_flags(["root_dir", "test_filepath", "checkpoint_path"], cfg)
-        output_dir = os.path.join(root_dir, "predictions")
+        output_dir = os.path.join(cfg.output_dir, "predictions")
         os.makedirs(output_dir, exist_ok=True)
         test_dataset = InstaGeoDataset(
             filename=test_filepath,
