87989627476005b826d40233fa9d3dec1b44092c
ahmedmokeddem <ja_mokeddem@esi.dz>
2025-02-04 23:16:45 +0100

    Fix: the new features

--- instageo/model/dataloader.py
+++ instageo/model/dataloader.py
@@ -19,8 +19,6 @@
 
 """Dataloader Module."""
 
-
-
 import os
 import random
 from functools import partial
@@ -31,12 +29,87 @@ import pandas as pd
 import rasterio
 import torch
 import xarray as xr
+from absl import logging
 from PIL import Image
 from rasterio.crs import CRS
 from torchvision import transforms
 from torch.utils.data import Dataset
 
-# Function to compute vegetation and soil indices
+
+def open_mf_tiff_dataset(
+    band_files: dict[str, Any], load_masks: bool
+) -> tuple[xr.Dataset, xr.Dataset | None, CRS]:
+    """Open multiple TIFF files as an xarray Dataset.
+
+    Args:
+        band_files (Dict[str, Dict[str, str]]): A dictionary mapping band names to file paths.
+        load_masks (bool): Whether or not to load the masks files.
+
+    Returns:
+        (xr.Dataset, xr.Dataset | None, CRS): A tuple of xarray Dataset combining data from all the
+            provided TIFF files, (optionally) the masks, and the CRS
+    """
+    band_paths = list(band_files["tiles"].values())
+    bands_dataset = xr.open_mfdataset(
+        band_paths,
+        concat_dim="band",
+        combine="nested",
+        mask_and_scale=False,  # Scaling will be applied manually
+    )
+    bands_dataset.band_data.attrs["scale_factor"] = 1
+    mask_paths = list(band_files["fmasks"].values())
+    mask_dataset = (
+        xr.open_mfdataset(
+            mask_paths,
+            concat_dim="band",
+            combine="nested",
+        )
+        if load_masks
+        else None
+    )
+    with rasterio.open(band_paths[0]) as src:
+        crs = src.crs
+    return bands_dataset, mask_dataset, crs
+
+
+
+
+
+
+
+
+def crop_array(
+    arr: np.ndarray, left: int, top: int, right: int, bottom: int
+) -> np.ndarray:
+    """Crop Numpy Image.
+
+    Crop a given array (image) using specified left, top, right, and bottom indices.
+
+    This function supports cropping both grayscale (2D) and color (3D) images.
+
+    Args:
+        arr (np.ndarray): The input array (image) to be cropped.
+        left (int): The left boundary index for cropping.
+        top (int): The top boundary index for cropping.
+        right (int): The right boundary index for cropping.
+        bottom (int): The bottom boundary index for cropping.
+
+    Returns:
+        np.ndarray: The cropped portion of the input array (image).
+
+    Raises:
+        ValueError: If the input array is not 2D or 3D.
+    """
+    if len(arr.shape) == 2:  # Grayscale image (2D array)
+        return arr[top:bottom, left:right]
+    elif len(arr.shape) == 3:  # Color image (3D array)
+        return arr[:, top:bottom, left:right]
+    elif len(arr.shape) == 4:  # Color image (3D array)
+        return arr[:, :, top:bottom, left:right]
+    else:
+        raise ValueError("Input array must be a 2D, 3D or 4D array")
+
+
 def compute_indices(image):
     """Compute NDVI, EVI, NDWI, and NDSI from Sentinel-2 bands."""
     B2, B3, B4, B8, B11, B12 = image
@@ -126,6 +199,31 @@ def process_and_augment(x, y, mean, std, im_size=224, augment=True):
 
     ims, label = normalize_and_convert_to_tensor(ims, label, mean, std)
     return ims, label
+
+# Function to process test-time images
+def process_test(x, y, mean, std, temporal_size=1, img_size=512, crop_size=224, stride=224):
+    preprocess_func = partial(
+        process_and_augment,
+        mean=mean,
+        std=std,
+        augment=False,
+    )
+
+    img_crops, mask_crops = [], []
+    width, height = img_size, img_size
+
+    for top in range(0, height - crop_size + 1, stride):
+        for left in range(0, width - crop_size + 1, stride):
+            bottom = top + crop_size
+            right = left + crop_size
+
+            img_crops.append(crop_array(x, left, top, right, bottom))
+            mask_crops.append(crop_array(y, left, top, right, bottom))
+
+    samples = [preprocess_func(x, y) for x, y in zip(img_crops, mask_crops)]
+    imgs = torch.stack([sample[0] for sample in samples])
+    labels = torch.stack([sample[1] for sample in samples])
+    return imgs, labels
 
 # PyTorch Dataset class
 class InstaGeoDataset(Dataset):
